2a94ddb4e6409e0ea50f9c37a7e1b9c4d7b045a0
Mark Jocel Piol <markjocel.piol@infor.com>
2024-09-11 17:32:49 +0800

    contact form, footer, etc

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactForm = new FormGroup({
+    fullName: new FormControl(''),
+    email: new FormControl(''),
+    contactNo: new FormControl(''),
+    service: new FormControl(''),
+    preferredDay: new FormControl(''),
+    preferredTime: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  contacts: {contact: string, link: string, icon: string}[] = [
+    {
+      contact:  "Find an adviser",
+      link: "/",
+      icon: "./facebook.png"
+    },
+    {
+      contact:  "Talk to an Accountant",
+      link: "/",
+      icon: "./facebook.png"
+    },
+    {
+      contact:  "Talk to a Mortgate broker",
+      link: "/",
+      icon: "./facebook.png"
+    },
+    {
+      contact:  "0800 700 699",
+      link: "/",
+      icon: "./facebook.png"
+    },
+    {
+      contact:  "reception@havenadvisers.co.nz",
+      link: "/",
+      icon: "./facebook.png"
+    },
+    {
+      contact:  "Be a part of our Facebook community",
+      link: "/",
+      icon: "./facebook.png"
+    }
+  ]
 }
